6377d9ae7492a16c5bb8f341b4e999859b8d9169
Surendar S <surs@akamai.com>
2021-04-03 16:15:00 +0530

    Integration of google books API
    1) Added methods to integrate google book API to fetch the books filtered by ISBN, author name and genre
    2) Added the API support in main class
    3) Added new API urls
    
    TO-DO:
    Refactor code to run the application as I've commented most of the code to make the API respond.
    Mostly crud.py and utils.py needs attention

--- services/app/services/book_details_module.py
+++ services/app/services/book_details_module.py
@@ -11,12 +11,19 @@ __status__ = "dev"
 from fastapi import FastAPI
 from typing import Optional
 import requests
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI(debug=True)
 
 URL = 'https://www.googleapis.com/books/v1/volumes'
 
-def get_books_by_isbn(isbn: str):
-    PARAMS = {'q': 'isbn' + isbn}
+def get_books_by_filter(filter: str, value: str):
+    PARAMS = {'q': filter + ":" + value}
+    logger.debug("Calling URL: {}".format(URL))
+    logger.debug("Filter by Param: {}".format(PARAMS))
     get_book = requests.get(url=URL, params=PARAMS)
+    logger.debug("Result: {}".format(get_book.json()))
     return get_book.json()
