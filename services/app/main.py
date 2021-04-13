629534d6be4696be7350b277ca365b3680705b08
Archana Thiruvaipati <archana.t.thiruvaipati@oracle.com>
2021-04-13 20:25:47 +0530

    Code changes for Issue 12#: All Get APIs fail with a 500 error for invalid Data, handle the exceptions gracefully and also added try-catch bloack for delete API

--- services/app/main.py
+++ services/app/main.py
@@ -48,19 +48,30 @@ async def save_book(book_data: BookSchema = Body(...)):
     return {"new book": new_entry}
 
 # Getting book by id from mongodb...
+# Getting book by id from mongodb...
 @app.get(urls.get_book_by_id_url)
 async def get_book_by_id(book_id: str):
-    #print(book_id)
-    book_found = await get_book(book_id)
-    if book_found:
-        return {"book": book_found}
+    logger.info("Fetching Book details for the ID {}".format(book_id))
+    try:
+        book_found = await get_book(book_id)
+        if book_found:
+            return {"book": book_found}
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return message
 
 # Deleting book data
 @app.delete(urls.delete_book_by_id_url)
 async def delete_book_by_id(book_id: str):
-    book_deleted = await delete_book(book_id)
-    if book_deleted:
-        return {"id": book_id, "message": "Deletion successful"}
+    try:
+        book_deleted = await delete_book(book_id)
+        if book_deleted:
+            return {"id": book_id, "message": "Deletion successful"}
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return message
 
 # Getting list of books from mongodb...
 @app.get(urls.get_all_books_url)
@@ -72,32 +83,47 @@ async def get_book_list():
 # Fetching book by ISBN...
 @app.get(urls.get_book_details_by_isbn)
 async def get_books_by_isbn(isbn):
-    logger.info("Fetching Book details for the ISBN {}".format(isbn))
-    item = book_details_module.get_books_by_filter('isbn', isbn)
-    if item:
-        return item
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return {"isbn": isbn,
+    try:
+        logger.info("Fetching Book details for the ISBN {}".format(isbn))
+        item = book_details_module.get_books_by_filter('isbn', isbn)
+        if item:
+            return item
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"isbn": isbn,
             "message": "book not found for given ISBN"}
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return message
 
 # Fetching book by Author name...
 @app.get(urls.get_book_details_by_author)
 async def get_books_by_author(author_name):
-    logger.info("Fetching Book details for the Author Name: \"{}\"".format(author_name))
-    item = book_details_module.get_books_by_filter('inauthor', author_name)
-    if item:
-        return item
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return {"author": author_name,
+    try:
+        logger.info("Fetching Book details for the Author Name: \"{}\"".format(author_name))
+        item = book_details_module.get_books_by_filter('inauthor', author_name)
+        if item:
+            return item
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"author": author_name,
             "message": "Either invalid author name or book not found for given Author Name"}
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return message
 
 # Fetching book by Author name...
 @app.get(urls.get_book_details_by_genre)
 async def get_books_by_genre(genre):
     logger.info("Fetching Book details for the genre: \"{}\"".format(genre))
-    item = book_details_module.get_books_by_filter('subject', genre)
-    if item:
-        return item
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return {"genre": genre,
+    try:
+        item = book_details_module.get_books_by_filter('subject', genre)
+        if item:
+            return item
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"genre": genre,
             "message": "Either invalid genre or book not found for given genre"}
+    except Exception as ex:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                return message
